66d8600ecb51cc87c0dbdafb4cdb24b8e2032f70
misshanya <mishanyadev@gmail.com>
2024-12-13 21:40:54 +0300

    Initial

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	serverPort := os.Getenv("SERVER_PORT")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	http.ListenAndServe(fmt.Sprintf(":%v", serverPort), r)
+}

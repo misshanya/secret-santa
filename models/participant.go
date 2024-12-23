cf0576928eeabf3d76343898613bc35e4d618ec3
misshanya <mishanyadev@gmail.com>
2024-12-24 00:08:57 +0300

    Delete my models because they are not needed

--- models/participant.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Participant struct {
-	ID     uint   `json:"id"`
-	UserID uint   `json:"user_id"`
-	RoomID uint   `json:"room_id"`
-	Wish   string `json:"wish"`
-}
--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       uint   `json:"id"`
-	Username string `json:"username"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
-}

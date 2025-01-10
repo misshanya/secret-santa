0f7d933a923295c221dddd1905c1ffc91aaaecb8
misshanya <mishanyadev@gmail.com>
2025-01-11 00:49:24 +0300

    Frontend: Add simple js functionality for registration

---
+++ frontend/js/register.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("form");
+const nameInput = document.querySelector("#name");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = {
+    name: nameInput.value,
+    username: usernameInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+
+  fetch("/api/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (response.status === 201) {
+        alert("Вы успешно зарегистрировались!");
+        window.location.href = "/login.html";
+      } else {
+        alert("Ошибка регистрации");
+        throw new Error("Ошибка регистрации");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.log("Ошибка регистрации:", error);
+      alert("Произошла ошибка при регистрации!");
+    });
+});
